93290799b0a8ec02bbacae31b305b81d89dce9d3
aAAiman <aimanreviva@gmail.com>
2025-05-26 23:10:13 +0700

    feat(signin): integrate backend API for user sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import LandingPage from './components/landing-page';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import SignIn from './components/SignIn';
+import { AuthProvider } from './components/AuthContext';
+import setupAxiosInterceptors from './components/axiosConfig';
+import { AuthContext } from './components/AuthContext';
 
 function AppWrapper() {
   const location = useLocation();
-
+  const { refreshAccessToken, logout } = useContext(AuthContext);
   const noNavFooterPaths = ['/signup', '/signin'];
-
   const hideNavFooter = noNavFooterPaths.includes(location.pathname);
+
+  useEffect(() => {
+    setupAxiosInterceptors(refreshAccessToken, logout);
+  }, [refreshAccessToken, logout]);
 
   return (
     <div className="bg-black min-h-screen">
@@ -29,11 +35,10 @@ function AppWrapper() {
 function App() {
   return (
     <Router>
-      <AppWrapper />
+      <AuthProvider>
+        <AppWrapper />
+      </AuthProvider>
     </Router>
   );
 }
-
-
-
 export default App;
